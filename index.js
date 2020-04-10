af79175c306c1592371c368f11bc2da3ed06b930
DavidAlbertoPe <david940103@gmail.com>
2020-04-10 15:20:49 -0500

    iniciando logica del juego

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 
 class Juego {
   constructor() {
-    this.inicializar()
+    this.inicializar();
+    this.generarSecuencia();
   }
-
   inicializar() {
-    btnEmpezar.classList.add('hide')
+    btnEmpezar.classList.add('hide');
+    this.nivel = 1;
+    //Creating object with the color buttoms
+    this.colores = {
+      celeste: celeste,
+      violeta: violeta,
+      naranja: naranja, 
+      verde: verde
+    }
+  }
+  
+  generarSecuencia(){
+    this.secuencia = new Array(10).fill(0);
+    this.secuencia = this.secuencia.map(n => Math.floor(Math.random() * 4));
   }
 }
 
 function empezarJuego() {
-  var juego = new Juego()
+  window.juego = new Juego();
 }
